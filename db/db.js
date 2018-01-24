0c68673c0598e0e24934e19f4fdab53e6ebf481e
Li Tianyi <noclyt@gmail.com>
2018-01-24 11:18:04 -0500

    remove db.js

--- db/db.js
+++
@@ -1,6 +0,0 @@
-var MongoClient = require('mongodb').MongoClient;
-var DB_CONN_STR = 'mongodb://localhost:27017/short-urls';
- 
-MongoClient.connect(DB_CONN_STR, function(err, db) {
-    console.log(" 连接成功！");
-});
